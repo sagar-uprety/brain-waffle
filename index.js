00054a316aa0b5df41935141b6ee34be59383cd5
sagar-uprety <sagarupreti100@gmail.com>
2022-02-06 20:30:12 +0545

    descriptions updated and three.js linked

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import App from "./src/app";
 
 document.addEventListener("DOMContentLoaded", () => {
-  if (window.location.pathname == "/entervr") {
-    let app = new App();
-    const indexContainer = document.querySelector(".content");
-    indexContainer.style.display = "none";
-    if (app) {
-      window.app = app;
-    }
+  // if (window.location.pathname == "/entervr"){
+  //     let app = new App();
+  //     const indexContainer = document.querySelector(".content");
+  //     indexContainer.style.display = "none";
+  //     if (app) {
+  //         window.app = app;
+  //     }
+  // }
+  let app;
+  const indexContainer = document.querySelector(".content");
+  switch (window.location.pathname) {
+    case "/entervr":
+      app = new App();
+      indexContainer.style.display = "none";
+      break;
+    default:
+      indexContainer.style.display = "";
+  }
+  if (app) {
+    window.app = app;
   }
 });
